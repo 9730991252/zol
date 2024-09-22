4db57be2a616d473740b2f7f350bf7c3b60d69eb
9730991252 <sunilkale101@gmail.com>
2024-09-22 11:47:06 +0530

    ok

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,50 +1,16 @@
-from channels.generic.websocket import WebsocketConsumer
-import json
-from asgiref.sync import async_to_sync
+from channels.consumer import SyncConsumer, AsyncConsumer
+from channels.exceptions import StopConsumer
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_code']
-        self.group_name = f'room_{self.room_name}'
-        async_to_sync(self.channel_layer.group_add)(
-            self.group_name,
-            self.channel_name
-        )
 
-        self.accept()
+class MySyncConsumer(SyncConsumer):
+    def websocket_connect(self, event):
+        print('websocket connected...', event)
+        self.send({
+            'type' : 'websocket.accept'
+        })
+    def websocket_receive(self, event):
+        print('message Recived from Client..', event)
 
-        #async_to_sync(self.channel_layer.group_send)(
-        #    f'room_{self.room_name}', {
-        #        'value' : json.dumps({'status' : 'online'})
-        #    }
-        #)
-
-        data = {'type': 'connected'}
-
-        self.send(text_data = json.dumps({
-            'payloade' : 'connected'
-        }))
-
-    def receive(self, text_data):
-        data = json.loads(text_data)
-
-        payload = {'message' : data.get('message') , 'sender' : data.get('sender')}
-
-        async_to_sync(self.channel_layer.group_send)(
-            f'room_{self.room_name}', {
-                'type' : 'send_message',
-                'value' : json.dumps(payload)
-            }
-        )
-
-
-    def disconnect(self , close_code):
-        pass
-
-    def send_message(self, text_data):
-        data = json.loads(text_data.get('value'))
-
-        self.send(text_data = json.dumps({
-            'payload' : data
-        }))
+    def websocket_disconnect(self, event):
+        print('websocket Disconnected...', event)
--- zol/asgi.py
+++ zol/asgi.py
@@ -1,30 +1,13 @@
-"""
-ASGI config for zol project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zol.settings')
 
-application = get_asgi_application()
-
-from channels.routing import ProtocolTypeRouter,URLRouter
-from django.urls import path
-from chat.consumers import *
-
-ws_pattern= [
-    path('chat/<room_code>', ChatConsumer.as_asgi())
-]
-
-application= ProtocolTypeRouter(
-    {
-        'websocket':(URLRouter(ws_pattern))
-    }
-)
+application = ProtocolTypeRouter({
+    'http':get_asgi_application(),
+    'websocket':URLRouter(
+        chat.routing.websocket_urlpatterns
+    )
+})
